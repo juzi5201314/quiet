6abb9023c8009bb3e3f151c6e9d4f588746565c4
Ghosin <admin@ghosin.com>
2020-03-22 22:46:16 +0800

    MongoDB 实现了新api

--- src/database/mongo.rs
+++ src/database/mongo.rs
@@ -19,15 +19,18 @@ impl Mongo {
             client: Client::with_uri_str(CONFIG.read().database_url.as_str())?,
         }))
     }
+    fn get_post_collection(&self) -> mongodb::Collection {
+        self.client
+            .database(DATABASE_NAME)
+            .collection(POST_COLLECTION)
+    }
 }
 
 use chrono::prelude::*;
 
 impl Database for Mongo {
     fn add_post(&self, title: String, content: String) -> Result<(), Error> {
-        self.client
-            .database(DATABASE_NAME)
-            .collection(POST_COLLECTION)
+        self.get_post_collection()
             .insert_one(
                 doc! {
                    "title": title,
@@ -41,12 +44,9 @@ impl Database for Mongo {
 
     fn get_posts(&self) -> Result<Vec<Post>, Error> {
         let mut posts = Vec::new();
-        let cursor = self
-            .client
-            .database(DATABASE_NAME)
-            .collection(POST_COLLECTION)
-            .find(None, None)?;
-        for result in cursor {
+        for result in self
+            .get_post_collection()
+            .find(None, None)? {
             match result {
                 Ok(document) => {
                     posts.push(Post {
@@ -88,9 +88,7 @@ impl Database for Mongo {
 
     fn get_post(&self, post_id: String) -> Result<Post, Error> {
         match self
-            .client
-            .database(DATABASE_NAME)
-            .collection(POST_COLLECTION)
+            .get_post_collection()
             .find_one(
                 doc! { "_id":  oid::ObjectId::with_string(post_id.as_str())? },
                 None,
@@ -127,14 +125,64 @@ impl Database for Mongo {
     }
 
     fn delete_post(&self, post_id: String) -> Result<(), Error> {
-        unimplemented!()
+        self.get_post_collection()
+            .delete_one(doc! { "_id": oid::ObjectId::with_string(post_id.as_str())? }, None)?;
+        Ok(())
     }
 
     fn update_post(&self, post_id: String, new_title: String, new_content: String) -> Result<(), Error> {
-        unimplemented!()
+        self.get_post_collection()
+            .update_one(doc! {"_id": oid::ObjectId::with_string(post_id.as_str())?},
+                        doc! {"$set": {"title": new_title, "content": new_content}}, None)?;
+        Ok(())
     }
 
     fn search_posts(&self, keyword: String) -> Result<Vec<Post>, Error> {
-        unimplemented!()
+        let mut posts = Vec::new();
+        for result in self
+            .get_post_collection()
+            .find(doc! {
+                    "$or": [
+                        {"content": {"$regex": keyword.as_str()}},
+                        {"title": {"$regex": keyword.as_str()}}
+                    ]
+                }, None)? {
+            match result {
+                Ok(document) => {
+                    posts.push(Post {
+                        id: document.get_object_id("_id").unwrap().to_hex(),
+                        title: if let Some(title) =
+                        document.get("title").and_then(Bson::as_str) {
+                            title.to_string()
+                        } else {
+                            String::from("unnamed")
+                        },
+                        content: if let Some(content) =
+                        document.get("content").and_then(Bson::as_str)
+                        {
+                            content.to_string()
+                        } else {
+                            String::new()
+                        },
+                        create_time: if let Some(create_time) =
+                        document.get("create_time").and_then(Bson::as_i64)
+                        {
+                            create_time
+                        } else {
+                            continue;
+                        },
+                        comments: if let Some(comments) =
+                        document.get("comments").and_then(Bson::as_i32)
+                        {
+                            Some(comments)
+                        } else {
+                            None
+                        },
+                    });
+                }
+                Err(e) => return Err(e.into()),
+            }
+        }
+        Ok(posts)
     }
 }
