e1135144ffb85cf5bc9b31f77033fd05d43317ce
juzi5201314 <1034236490@qq.com>
2020-03-23 20:41:12 +0800

    完成delete post。 update没写完，傻逼diesel我炸了

--- src/database/sqlite.rs
+++ src/database/sqlite.rs
@@ -1,9 +1,7 @@
-use diesel::{
-    r2d2 as diesel_r2d2, r2d2::ConnectionManager, QueryDsl, RunQueryDsl, SqliteConnection,
-};
+use diesel::{r2d2 as diesel_r2d2, r2d2::ConnectionManager, QueryDsl, RunQueryDsl, SqliteConnection, r2d2::PooledConnection, ExpressionMethods, Column, AppearsOnTable, QueryResult};
 
 use crate::database::models::{NewPost, Post};
-use crate::database::{Database, Error};
+use crate::database::{Database, Error, ErrorKind};
 use crate::CONFIG;
 
 #[derive(Clone)]
@@ -24,7 +22,16 @@ impl Sqlite {
     }
 }
 
+impl Sqlite {
+    fn get_conn(&self) -> Result<PooledConnection<ConnectionManager<SqliteConnection>>, Error> {
+        Ok(self.pool.get()?)
+    }
+}
+
 use super::schema::posts;
+use diesel::query_builder::{AsChangeset, QueryFragment, AstPass};
+use diesel::backend::Backend;
+
 
 impl Database for Sqlite {
     fn add_post(&self, title: String, content: String) -> Result<(), Error> {
@@ -35,21 +42,37 @@ impl Database for Sqlite {
     }
 
     fn get_posts(&self) -> Result<Vec<Post>, Error> {
-        let conn = self.pool.get()?;
-        Ok(posts::table.load::<Post>(&conn)?)
+        Ok(posts::table.load::<Post>(&self.get_conn()?)?)
     }
 
     fn get_post(&self, post_id: String) -> Result<Post, Error> {
-        let conn = self.pool.get()?;
-        Ok(posts::table.find(post_id).first(&conn)?)
+        Ok(posts::table.find(post_id).first(&self.get_conn()?)?)
     }
 
     fn delete_post(&self, post_id: String) -> Result<(), Error> {
-        unimplemented!()
+        let del_num = diesel::delete(posts::table.find(post_id)).execute(&self.get_conn()?)?;
+        if del_num != 1 {
+            Err(Error(ErrorKind::Other, String::from("delete result not equal 1.")))
+        } else {
+            Ok(())
+        }
     }
 
-    fn update_post(&self, post_id: String, new_title: String, new_content: String) -> Result<(), Error> {
-        unimplemented!()
+    fn update_post(&self, post_id: String, title: Option<String>, content: Option<String>) -> Result<(), Error> {
+        unimplemented!();
+        /*if title.is_none() && content.is_none() {
+            return Err(Error(ErrorKind::Other, String::from("no update.")))
+        }
+
+        let uptime = posts::update_time.eq(chrono::Local::now().timestamp());
+
+        let data = Updates(vec![uptime]);
+
+        if diesel::update(posts::table.find(post_id)).set(data).execute(&self.get_conn()?)? != 1 {
+            Err(Error(ErrorKind::Other, String::from("update result not equal 1.")))
+        } else {
+            Ok(())
+        }*/
     }
 
     fn search_posts(&self, keyword: String) -> Result<Vec<Post>, Error> {
