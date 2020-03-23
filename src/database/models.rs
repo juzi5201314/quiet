6a85579bea622f04da995e60aa98d06be34a2361
juzi5201314 <1034236490@qq.com>
2020-03-23 20:40:03 +0800

    添加update time字段，id修改为text

--- src/database/models.rs
+++ src/database/models.rs
@@ -8,23 +8,29 @@ pub struct Post {
     pub title: String,
     pub content: String,
     pub create_time: i64,
+    pub update_time: i64,
     pub comments: Option<i32>,
 }
 
 #[derive(Insertable)]
 #[table_name = "posts"]
 pub struct NewPost {
+    pub id: String,
     pub title: String,
     pub content: String,
+    pub update_time: i64,
     pub create_time: i64,
 }
 
 impl NewPost {
     pub fn new(title: String, content: String) -> Self {
+        let now = chrono::Local::now().timestamp();
         NewPost {
+            id: uuid::Uuid::new_v4().to_string(),
             title,
             content,
-            create_time: chrono::Local::now().timestamp(),
+            update_time: now,
+            create_time: now,
         }
     }
 }
--- src/database/schema.rs
+++ src/database/schema.rs
@@ -4,6 +4,7 @@ table! {
         title -> Text,
         content -> Text,
         create_time -> BigInt,
+        update_time -> BigInt,
         comments -> Nullable<Integer>,
     }
 }
